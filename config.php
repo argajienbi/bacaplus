381b91a2017784f2d718b014a23b37cfec653721
argajienbi <armin.gandi@gmail.com>
2019-01-14 11:52:45 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_2264665873544852";
+$raw_uid="09ab36c1-720e-4ac7-8bb7-556eea9ce349";
+$user_name="Armin+Gandi";
+$invite_code="FAN4iBZM6m";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="b018e43cc5d256d0";
+$device_type="Xiaomi++Redmi+3";
+
+
+?>
